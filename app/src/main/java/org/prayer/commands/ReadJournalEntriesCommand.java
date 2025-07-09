96980ba99ab72072a398d7a6473cef6d8bc93559
Jake <waters.1909@gmail.com>
2025-07-08 23:58:51 -0600

    separating formatting into different class

--- app/src/main/java/org/prayer/commands/ReadJournalEntriesCommand.java
+++ app/src/main/java/org/prayer/commands/ReadJournalEntriesCommand.java
@@ -16,16 +16,6 @@ import org.apache.avro.generic.GenericDatumReader;
 
 
 public class ReadJournalEntriesCommand extends Command {
-	public static final String ANSI_RESET = "\u001B[0m";
-	public static final String ANSI_BLACK = "\u001B[30m";
-	public static final String ANSI_RED = "\u001B[31m";
-	public static final String ANSI_GREEN = "\u001B[32m";
-	public static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String ANSI_BLUE = "\u001B[34m";
-	public static final String ANSI_PURPLE = "\u001B[35m";
-	public static final String ANSI_CYAN = "\u001B[36m";
-	public static final String ANSI_WHITE = "\u001B[37m";
-
 	public ReadJournalEntriesCommand (String[] args) {
 		super(args);
 		this.setCommand("read-journal-entries");
@@ -49,10 +39,10 @@ public class ReadJournalEntriesCommand extends Command {
 					GenericRecord entry = null;
 					while (dataFileReader.hasNext()) {
 						entry = dataFileReader.next(entry);
-						String idOutput = String.format("\n%sentry %s", ReadJournalEntriesCommand.ANSI_YELLOW, entry.get("entryId"));
-						String timestampOutput = String.format("\n%sDate: %s", ReadJournalEntriesCommand.ANSI_WHITE, entry.get("entryTimestamp"));
-						String textOutput = String.format("\n\n  %s%s", ReadJournalEntriesCommand.ANSI_CYAN, entry.get("text"));
-						String output = String.format("%s%s%s%s", idOutput, timestampOutput, textOutput, ReadJournalEntriesCommand.ANSI_RESET);
+						String idOutput = String.format("\n%sentry %s", Formatting.ANSI_YELLOW, entry.get("entryId"));
+						String timestampOutput = String.format("\n%sDate: %s", Formatting.ANSI_WHITE, entry.get("entryTimestamp"));
+						String textOutput = String.format("\n\n  %s%s", Formatting.ANSI_CYAN, entry.get("text"));
+						String output = String.format("%s%s%s%s", idOutput, timestampOutput, textOutput, Formatting.ANSI_RESET);
 						System.out.println(output);
 					}
 				} catch (IOException e) {
