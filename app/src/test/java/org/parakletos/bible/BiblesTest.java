c1546440af2e60e9d43246ce653b2be3c10d51ff
Jake <waters.1909@gmail.com>
2025-08-16 19:23:32 -0600

    adding test for bible versions

---
+++ app/src/test/java/org/parakletos/bible/BiblesTest.java
@@ -0,0 +1,38 @@
+package org.parakletos;
+
+// junit
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+// java
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.reflect.Field;
+// jackson
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class BiblesTest {
+	@Test
+	void DoAllBibleVersionOptionsExist() {
+		BibleSuperSearchApi api = new BibleSuperSearchApi();
+		JsonNode bibles = api.getBibles();
+
+		// bible version options
+		boolean pass = true;
+		List<String> versionsThatDoNotExist = new ArrayList<>();
+		for (Field field: new Bibles().getClass().getDeclaredFields()) {
+			field.setAccessible(true);
+			
+			// check if the name exists in the response for all bibles
+			//System.out.println(bibles.path(field.getName()));
+			String bibleVersion = field.getName().toLowerCase();
+			if (!bibles.has(bibleVersion)) {
+				pass = false;
+				versionsThatDoNotExist.add(bibleVersion);
+			}
+		}
+		
+		// assert and create message for values that don't exist if applicable
+		String message = String.format("The following Bible version don't exist: [%s]", String.join(",", versionsThatDoNotExist));
+		assertEquals(true, pass, message);
+	}
+}
